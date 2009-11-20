ce821ea9f7e9c11e44f0a5de5c8d14b2db8ac606
Bart Nagel <bjn@ecs.soton.ac.uk>
2009-11-20 11:37:53 +0000

    ignore swap files

--- webtool/sync.sh
+++ webtool/sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-unison -auto -root . -root ssh://lslvm-bjn1//srv/easihe/www/authoringtool -ignore "Path sync.sh"
+unison -auto -root . -root ssh://lslvm-bjn1//srv/easihe/www/authoringtool -ignore "Path sync.sh" -ignore "Regex .*.swp$"
