6af13fbc80e199a6000a588308a3ee7c0f9f4a22
Bart Nagel <bjn@ecs.soton.ac.uk>
2009-11-20 16:24:41 +0000

    updated sync script to ignore git files

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-unison -auto -root . -root ssh://lslvm-bjn1//srv/easihe/www/authoringtool -ignore "Path sync.sh" -ignore "Regex .*.swp$"
+unison -auto -root . -root ssh://lslvm-bjn1//srv/easihe/www/authoringtool -ignore "Path sync.sh" -ignore "Regex .*.swp$" -ignore "Path .git" -ignore "Path .gitignore"
