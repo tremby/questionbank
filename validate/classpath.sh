9bc2e0843ab714e3c13c4ee9836dcac6f80391b1
Bart Nagel <bjn@ecs.soton.ac.uk>
2009-11-20 16:32:06 +0000

    added SLF4J jars to the lib dir
    since they aren't packaged in Ubuntu Hardy

--- validate/classpath.sh
+++ validate/classpath.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 CLASSPATH="."
-CLASSPATH="$CLASSPATH:/usr/share/java/slf4j-api.jar:/usr/share/java/slf4j-nop.jar"
 for JAR in $(find lib -iname "*.jar"); do
 	CLASSPATH="$CLASSPATH:$JAR"
 done
