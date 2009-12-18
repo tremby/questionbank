6a369b8bbdc4808516ada98a6582e3e93d471a5a
Bart Nagel <bjn@ecs.soton.ac.uk>
2009-12-18 17:38:26 +0000

    added temp debug info to the footer

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -1,2 +1,11 @@
+<h3>Debug stuff</h3>
+<pre style="height: 300px; overflow: auto; background-color: #eeffee;">
+Post:
+<?php echo htmlspecialchars(print_r($_POST, true)); ?>
+
+
+Session:
+<?php echo htmlspecialchars(print_r($_SESSION, true)); ?>
+</pre>
 </body>
 </html>
