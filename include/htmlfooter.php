ee195e7f98a0928ccfd8049c854f0cfb300e0425
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-01-04 15:07:19 +0000

    display debug info only to my IP

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -1,11 +1,13 @@
-<h3>Debug stuff</h3>
-<pre style="height: 300px; overflow: auto; background-color: #eeffee;">
+<?php if ($_SERVER["REMOTE_ADDR"] == "152.78.64.88") { ?>
+	<h3>Debug stuff</h3>
+	<pre style="height: 300px; overflow: auto; background-color: #eeffee;">
 Post:
 <?php echo htmlspecialchars(print_r($_POST, true)); ?>
 
 
 Session:
 <?php echo htmlspecialchars(print_r($_SESSION, true)); ?>
+<?php } ?>
 </pre>
 </body>
 </html>
