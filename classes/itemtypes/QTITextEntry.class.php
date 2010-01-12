375f088b62ba2bde9f51010bb149d887db7b9da7
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-01-12 18:01:02 +0000

    text entry: added client side error checking and added default handlers

--- classes/itemtypes/QTITextEntry.class.php
+++ classes/itemtypes/QTITextEntry.class.php
@@ -238,9 +238,104 @@ class QTITextEntry extends QTIAssessmentItem {
 				});
 			};
 
+			submitcheck = function() {
+				// ensure the gaps table is up to date
+				updategapstable();
+
+				// clear any previously set background colours
+				$("input, textarea").removeClass("error warning");
+
+				// title must be set
+				if ($("#title").val().length == 0) {
+					$("#title").addClass("error");
+					alert("A title must be set for this item");
+					return false;
+				}
+
+				// must have at least one gap
+				if ($("div.gap:visible").size() == 0) {
+					$("#textbody").addClass("error");
+					alert("You must have at least one gap for the candidate to fill in");
+					return false;
+				}
+
+				// scores must make sense
+				var ok = true;
+				$("input.responsescore:visible").each(function() {
+					if ($(this).val().length == 0 || isNaN($(this).val()) || parseFloat($(this).val()) < 0) {
+						var gapid = parseInt($(this).attr("id").split("_")[1]);
+						var responseid = parseInt($(this).attr("id").split("_")[3]);
+						$(this).addClass("error");
+						alert("Score for gap " + (gapid + 1) + " response " + (responseid + 1) + " must be a positive number");
+						ok = false;
+						return false;
+					}
+				});
+				if (!ok) return false;
+
+				// can't have identical responses for a single gap
+				for (var gap = 0; gap < $("div.gap:visible").size(); gap++) {
+					for (var i = 0; i < $("#gap_" + gap + " input.responsetext").size(); i++) {
+						for (var j = i + 1; j < $("#gap_" + gap + " input.responsetext").size(); j++) {
+							if ($("#gap_" + gap + "_response_" + i).val() == $("#gap_" + gap + "_response_" + j).val()) {
+								$("#gap_" + gap + "_response_" + i + ", #gap_" + gap + "_response_" + j).addClass("error");
+								alert("No two responses can be the same but gap " + (gap + 1) + " responses " + (i + 1) + " and " + (j + 1) + " are equal");
+								return false;
+							}
+						}
+					}
+				};
+
+				// issue warnings if applicable
+
+				// confirm the user wanted an empty stimulus
+				if ($("#stimulus").val().length == 0) {
+					$("#stimulus").addClass("warning");
+					if (!confirm("Stimulus is empty -- click OK to continue regardless or cancel to edit it"))
+						return false;
+					else
+						$("#stimulus").removeClass("error warning");
+				}
+
+				// confirm the user wanted any empty boxes
+				$("input.responsetext:visible").each(function(n) {
+					if ($(this).val().length == 0) {
+						var gapid = parseInt($(this).attr("id").split("_")[1]);
+						var responseid = parseInt($(this).attr("id").split("_")[3]);
+						$(this).addClass("warning");
+						ok = confirm("Gap " + (gapid + 1) + " response " + (responseid + 1) + " is empty -- click OK to continue regardless or cancel to edit it");
+						if (ok)
+							$(this).removeClass("error warning");
+						else
+							return false; //this is "break" in the Jquery each() pseudoloop
+					}
+				});
+				if (!ok) return false;
+
+				// confirm the user wanted zero scores
+				$("input.responsescore:visible").each(function(n) {
+					if (parseFloat($(this).val()) == 0.0) {
+						var gapid = parseInt($(this).attr("id").split("_")[1]);
+						var responseid = parseInt($(this).attr("id").split("_")[3]);
+						$(this).addClass("warning");
+						ok = confirm("Score for gap " + (gapid + 1) + " response " + (responseid + 1) + " is zero but this is the default score for any response not listed -- click OK to continue regardless or cancel to edit it");
+						if (ok)
+							$(this).removeClass("error warning");
+						else
+							return false; //this is "break" in the Jquery each() pseudoloop
+					}
+				});
+				if (!ok) return false;
+
+				return true;
+			};
+
 			$(document).ready(function() {
 				$("#textbody").change(updategapstable);
-				// TODO: default handlers for existing gap table button/fields
+				$("input.addresponse:visible").click(addresponse);
+				$("input.removeresponse:visible").click(removeresponse);
+				$("input.responsetext:visible").change(updatetextgap);
+				$("#submit").click(submitcheck);
 			});
 		</script>
 
