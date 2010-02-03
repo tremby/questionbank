4e859998c8a6d76147d2a72eb657ff6f0a2e29e6
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-02-03 13:00:45 +0000

    MC/MR: removed duplicate title and stimulus fields

--- classes/QTIMultipleChoiceResponse.class.php
+++ classes/QTIMultipleChoiceResponse.class.php
@@ -335,12 +335,6 @@ abstract class QTIMultipleChoiceResponse extends QTIAssessmentItem {
 			</label></ul>
 		</dd>
 
-		<dt><label for="title">Title</label></dt>
-		<dd><input size="64" type="text" name="title" id="title"<?php if (isset($this->data["title"])) { ?> value="<?php echo htmlspecialchars($this->data["title"]); ?>"<?php } ?>></dd>
-
-		<dt><label for="stimulus">Stimulus</label></dt>
-		<dd><textarea class="qtitinymce resizable" rows="8" cols="64" name="stimulus" id="stimulus"><?php if (isset($this->data["stimulus"])) echo htmlspecialchars($this->data["stimulus"]); ?></textarea></dd>
-
 		<dt><label for="prompt">Question prompt</label></dt>
 		<dd><textarea rows="2" cols="64" name="prompt" id="prompt"><?php if (isset($this->data["prompt"])) echo htmlspecialchars($this->data["prompt"]); ?></textarea></dd>
 
