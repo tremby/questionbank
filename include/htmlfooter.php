da2fe47a16a5852120003102a8d287dbaf9d1323
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-02-05 10:44:56 +0000

    include _FILES in debug data

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -1,6 +1,10 @@
 <?php if ($_SERVER["REMOTE_ADDR"] == "152.78.64.88") { ?>
 	<h3>Debug stuff</h3>
 	<pre style="height: 300px; overflow: auto; background-color: #eeffee;">
+Files:
+<?php echo htmlspecialchars(print_r($_FILES, true)); ?>
+
+
 Post:
 <?php echo htmlspecialchars(print_r($_POST, true)); ?>
 
