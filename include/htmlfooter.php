653b1bacdff4d305f4328e69f0b203e62191ebc8
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-02-08 13:47:51 +0000

    removed debug stuff from the footer

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -1,17 +1,2 @@
-<?php if ($_SERVER["REMOTE_ADDR"] == "152.78.64.88") { ?>
-	<h3>Debug stuff</h3>
-	<pre style="height: 300px; overflow: auto; background-color: #eeffee;">
-Files:
-<?php echo htmlspecialchars(print_r($_FILES, true)); ?>
-
-
-Post:
-<?php echo htmlspecialchars(print_r($_POST, true)); ?>
-
-
-Session:
-<?php echo htmlspecialchars(print_r($_SESSION, true)); ?>
-<?php } ?>
-</pre>
 </body>
 </html>
