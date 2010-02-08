37eb33a804b40b9b41f8d8214858e6da79811d28
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-02-08 13:50:33 +0000

    removed sync script

--- sync.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-unison -auto -root . -root ssh://lslvm-bjn1//srv/easihe/www/authoringtool -ignore "Path sync.sh" -ignore "Regex .*.swp$" -ignore "Path .git" -ignore "Path .gitignore" -merge "Name * -> urxvt -e vimdiff CURRENT1 CURRENT2"
