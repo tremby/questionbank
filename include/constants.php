be8a1d41586154e19b0276a71533c83afbb9ac6b
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-02-10 11:32:52 +0000

    more sophisticated path detection

--- include/constants.php
+++ include/constants.php
@@ -11,8 +11,20 @@ Licensed under the Creative Commons 'Attribution non-commercial share alike'
 licence -- see the LICENCE file for more details
 ------------------------------------------------------------------------------*/
 
-define("SITEROOT_WEB", "/authoringtool/");
-define("SITEROOT_LOCAL", $_SERVER["DOCUMENT_ROOT"] . "/authoringtool/");
+// filesystem path to the eqiat root directory -- one level above this file, 
+// ending in a trailing slash
+define("SITEROOT_LOCAL", dirname(dirname(__FILE__)) . "/");
+
+// query path to the eqiat root directory ending in a trailing slash -- makes an 
+// absolute URL to the main page
+define("SITEROOT_WEB", "/" . substr(SITEROOT_LOCAL, strlen($_SERVER["DOCUMENT_ROOT"])));
+
+// TODO: Ensure the above two give the expected results on different server 
+// types (Windows? Lighttpd?). Expected values:
+// SITEROOT_LOCAL (should this have backslashes if Windows?)
+// 	/var/www/eqiat/ (or /var/www/ if not running in a subdirectory)
+// SITEROOT_WEB
+// 	/eqiat/ (or / if not running in a subdirectory)
 
 // namespaces
 define("NS_IMSQTI", "http://www.imsglobal.org/xsd/imsqti_v2p1");
