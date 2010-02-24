5ee3fe3349048c875a83458885108e32cc04b0d8
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-02-24 14:44:57 +0000

    added links to footer

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -14,7 +14,12 @@ licence -- see the LICENCE file for more details
 ?>
 </div>
 <div id="footer">
-	<?php echo PROGRAMNAME; ?> <?php echo VERSION; ?>, &copy; 2010 JISC-funded EASiHE project, University of Southampton
+	<a href="http://github.com/tremby/eqiat"><?php echo PROGRAMNAME; ?></a>
+	<?php echo VERSION; ?>,
+	&copy; 2010
+	<a href="http://www.jisc.ac.uk">JISC</a>-funded
+	<a href="http://easihe.ecs.soton.ac.uk">EASiHE project</a>,
+	<a href="http://www.soton.ac.uk">University of Southampton</a>
 	<?php if (strpos(VERSION, "git") !== false) { ?>
 		<!-- output of `git show | head -5`: <?php echo "\n" . `git show | head -5`; ?>
 		-->
