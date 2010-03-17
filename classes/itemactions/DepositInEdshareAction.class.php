a076eb372b8da312453d5c83ada8e00a664c79ee
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-03-17 01:07:27 +0000

    much improved Edshare uploader
    username and password are stored temporarily in session
    Edshare is queried for available collections, user can choose between them
    other repository information is displayed

--- classes/itemactions/DepositInEdshareAction.class.php
+++ classes/itemactions/DepositInEdshareAction.class.php
@@ -17,6 +17,12 @@ if (DIEA_AVAILABLE && !defined("DIEA_EDSHARE_HOST"))
 class DepositInEdshareAction extends ItemAction {
 	private $ai;
 	private $errors = array();
+	private $repoidxml = null;
+	private $reponame = null;
+	private $repocontent = null;
+	private $repometadatapolicy = null;
+	private $repodatapolicy = null;
+	private $reposubmissionpolicy = null;
 
 	public function name() {
 		return "deposit in Edshare";
@@ -31,34 +37,103 @@ class DepositInEdshareAction extends ItemAction {
 	}
 
 	public function getLogic() {
+		// get login details if we don't already have them
+		if (!$this->haveLogin()) {
+			$this->getLogin();
+			return;
+		}
+
+		// get list of collections we can deposit to
+		$servicedocxml = simplexml_load_string($_SESSION["diea_servicedocxml"]);
+		$collections = array();
+		foreach ($servicedocxml->workspace->collection as $collection) {
+			$acceptzip = false;
+			foreach ($collection->accept as $accept) {
+				if ((string) $accept == "application/zip") {
+					$acceptzip = true;
+					break;
+				}
+			}
+			if (!$acceptzip)
+				continue;
+			$coll = array("href" => (string) $collection["href"]);
+
+			$children = $collection->children("http://www.w3.org/2005/Atom");
+			foreach ($children as $child) {
+				if ($child->getName() == "title") {
+					$coll["title"] = (string) $child;
+					break;
+				}
+			}
+
+			$children = $collection->children("http://purl.org/net/sword/");
+			foreach ($children as $child) {
+				if ($child->getName() == "collectionPolicy")
+					$coll["collectionPolicy"] = (string) $child;
+				else if ($child->getName() == "treatment")
+					$coll["treatment"] = (string) $child;
+			}
+
+			$children = $collection->children("http://purl.org/dc/terms/");
+			foreach ($children as $child) {
+				if ($child->getName() == "abstract") {
+					$coll["abstract"] = (string) $child;
+					break;
+				}
+			}
+
+			$collections[] = $coll;
+		}
+
+		// show the deposit form
 		$GLOBALS["title"] = "Deposit item in Edshare";
 		include "htmlheader.php";
 		?>
 		<h2>Deposit item "<?php echo htmlspecialchars($this->ai->data("title")); ?>" in Edshare</h2>
+
 		<?php if (!empty($this->errors)) showmessages($this->errors, "Error" . plural($this->errors), "error"); ?>
-		<p>Before the item can be deposited you need to provide your <a 
-		href="http://<?php echo DIEA_EDSHARE_HOST; ?>">Edshare</a> 
-		username and password. This information is not stored.</p>
+
 		<form action="<?php echo $this->actionURL($this->ai); ?>" method="post">
 			<dl>
-				<dt><label for="diea_username">Username</label></dt>
-				<dd><input type="text" size="64" name="diea_username" id="diea_username"<?php if (isset($_POST["diea_username"])) { ?> value="<?php echo htmlspecialchars($_POST["diea_username"]); ?>"<?php } ?>></dd>
-
-				<dt><label for="diea_password">Password</label></dt>
-				<dd><input type="password" size="64" name="diea_password" id="diea_password"></dd>
+				<dt>Collection</dt>
+				<dd>
+					<dl>
+						<?php foreach($collections as $collection) { ?>
+							<dt><label>
+							<input type="radio" name="collection" value="<?php echo $collection["href"]; ?>"<?php if (isset($_POST["collection"]) && $_POST["collection"] == $collection["href"]) { ?> checked="checked"<?php } ?>>
+								<?php echo htmlspecialchars($collection["title"]); ?>
+							</label></dt>
+							<dd>
+								<?php if (isset($collection["abstract"])) { ?><div class="abstract"><?php echo htmlspecialchars($collection["abstract"]); ?></div><?php } ?>
+								<?php if (isset($collection["collectionPolicy"])) { ?><div class="collectionpolicy"><?php echo htmlspecialchars($collection["collectionPolicy"]); ?></div><?php } ?>
+								<?php if (isset($collection["treatment"])) { ?><div class="treatment"><?php echo htmlspecialchars($collection["treatment"]); ?></div><?php } ?>
+							</dd>
+						<?php } ?>
+					</dl>
+				</dd>
 
 				<dt></dt>
 				<dd><input type="submit" name="deposit" value="Deposit"></dd>
 			</dl>
 		</form>
 		<?php
+		echo $this->repoInfo();
 		include "htmlfooter.php";
 	}
 
 	public function postLogic() {
-		// check a username and password were given
-		if (!isset($_POST["diea_username"]) || empty($_POST["diea_username"]) || !isset($_POST["diea_password"]) || empty($_POST["diea_password"])) {
-			$this->errors[] = "Edshare username or password not given";
+		// get login details if we don't already have them
+		if (!$this->haveLogin()) {
+			$this->postLogin();
+			return;
+		}
+
+		// clear errors
+		$this->errors = array();
+
+		// check input
+		if (!isset($_POST["collection"])) {
+			$this->errors[] = "No collection specified";
 			$this->getLogic();
 			return;
 		}
@@ -93,25 +168,27 @@ class DepositInEdshareAction extends ItemAction {
 
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
-			CURLOPT_URL				=>	"http://" . DIEA_EDSHARE_HOST . "/sword-app/deposit/inbox",
+			CURLOPT_URL				=>	$_POST["collection"],
 			CURLOPT_POST			=>	true,
 			CURLOPT_HEADER			=>	true,
-			CURLOPT_USERPWD			=>	$_POST["diea_username"] . ":" . $_POST["diea_password"],
+			CURLOPT_USERPWD			=>	$_SESSION["diea_username"] . ":" . $_SESSION["diea_password"],
 			CURLOPT_RETURNTRANSFER	=>	true,
 			CURLOPT_POSTFIELDS		=>	simplexml_indented_string($ep3),
 			CURLOPT_HTTPHEADER		=>	array(
-				"Content-Type: application/xml", //this may need to be text/xml
+				"Content-Type: application/xml",
 				"X-Packaging: http://eprints.org/ep2/data/2.0",
 				"Expect: ",
 			),
 		));
 		$response = curl_exec($curl);
 		$responseinfo = curl_getinfo($curl);
+		$code = $responseinfo["http_code"];
 		$headers = response_headers($response);
 		$body = response_body($response);
 
-		switch ($responseinfo["http_code"]) {
+		switch ($code) {
 			case 401:
+				unset($_SESSION["diea_username"], $_SESSION["diea_password"]);
 				$this->errors[] = "There was an authorization problem" . (isset($headers["X-Error-Code"]) ? ": " . $headers["X-Error-Code"] : "");
 				$this->getLogic();
 				return;
@@ -146,12 +223,174 @@ class DepositInEdshareAction extends ItemAction {
 					$this->errors[] = "$k: $v";
 				$this->getLogic();
 				return;
-				break;
 		}
 	}
 
 	public function available(QTIAssessmentItem $ai) {
 		return DIEA_AVAILABLE && $ai->getQTI() && !count($ai->getErrors());
+	}
+
+	private function repoIDXML() {
+		// get identification from Edshare host if we haven't already
+		if (is_null($this->repoidxml))
+			$this->repoidxml = simplexml_load_file("http://" . DIEA_EDSHARE_HOST . "/cgi/oai2?verb=Identify");
+
+		return $this->repoidxml;
+	}
+
+	// return the repository name
+	private function repoName() {
+		return (string) $this->repoIDXML()->Identify->repositoryName;
+	}
+
+	// populate properties we can get without logging in
+	private function getRepoDescription() {
+		if (!is_null($this->repocontent))
+			return;
+
+		foreach ($this->repoIDXML()->Identify->description as $description) {
+			if (!count($description->eprints))
+				continue;
+
+			$this->repocontent = (string) $description->eprints->content->text;
+			$this->repometadatapolicy = (string) $description->eprints->metadataPolicy->text;
+			$this->repodatapolicy = (string) $description->eprints->dataPolicy->text;
+			$this->reposubmissionpolicy = (string) $description->eprints->submissionPolicy->text;
+			return;
+		}
+	}
+
+	// get those properties
+	private function repoContent() {
+		$this->getRepoDescription();
+		return $this->repocontent;
+	}
+	private function repoMetadataPolicy() {
+		$this->getRepoDescription();
+		return $this->repometadatapolicy;
+	}
+	private function repoDataPolicy() {
+		$this->getRepoDescription();
+		return $this->repodatapolicy;
+	}
+	private function repoSubmissionPolicy() {
+		$this->getRepoDescription();
+		return $this->reposubmissionpolicy;
+	}
+
+	// return an HTML list of repo info we can get without logging in
+	private function repoInfo() {
+		ob_start();
+		?>
+		<h2>Repository information</h2>
+		<dl>
+			<dt>Name</dt>
+			<dd><?php echo htmlspecialchars($this->repoName()); ?></dd>
+
+			<dt>URL</dt>
+			<dd><a href="http://<?php echo DIEA_EDSHARE_HOST; ?>">http://<?php echo DIEA_EDSHARE_HOST; ?></a></dd>
+
+			<dt>Description</dt>
+			<dd><?php echo htmlspecialchars($this->repoContent()); ?></dd>
+
+			<dt>Metadata policy</dt>
+			<dd><?php echo htmlspecialchars($this->repoMetadataPolicy()); ?></dd>
+
+			<dt>Data policy</dt>
+			<dd><?php echo htmlspecialchars($this->repoDataPolicy()); ?></dd>
+
+			<dt>Submission policy</dt>
+			<dd><?php echo htmlspecialchars($this->repoSubmissionPolicy()); ?></dd>
+		<?php
+		return ob_get_clean();
+	}
+
+	// return true if we have good login details
+	private function haveLogin() {
+		return isset($_SESSION["diea_username"]);
+	}
+
+	// give an HTML form to collect login details
+	private function getLogin() {
+		$GLOBALS["title"] = "Deposit item in Edshare – login details required";
+		include "htmlheader.php";
+		?>
+		<h2>Deposit item "<?php echo htmlspecialchars($this->ai->data("title")); ?>" in Edshare</h2>
+
+		<?php if (!empty($this->errors)) showmessages($this->errors, "Error" . plural($this->errors), "error"); ?>
+
+		<p>Before the item can be deposited you need to provide your username 
+		and password for the repository. This information is stored only for 
+		this session.</p>
+		<form action="<?php echo $this->actionURL($this->ai); ?>" method="post">
+			<dl>
+				<dt><label for="diea_username">Username</label></dt>
+				<dd><input type="text" size="64" name="diea_username" id="diea_username"<?php if (isset($_POST["diea_username"])) { ?> value="<?php echo htmlspecialchars($_POST["diea_username"]); ?>"<?php } ?>></dd>
+
+				<dt><label for="diea_password">Password</label></dt>
+				<dd><input type="password" size="64" name="diea_password" id="diea_password"></dd>
+
+				<dt></dt>
+				<dd><input type="submit" name="edsharelogin" value="Log in"></dd>
+			</dl>
+		</form>
+		<?php
+		echo $this->repoInfo();
+		include "htmlfooter.php";
+	}
+
+	// handle the login details form being posted
+	private function postLogin() {
+		// clear errors
+		$this->errors = array();
+
+		// check a username and password were given
+		if (!isset($_POST["diea_username"]) || empty($_POST["diea_username"]) || !isset($_POST["diea_password"]) || empty($_POST["diea_password"])) {
+			$this->errors[] = "Edshare username or password not given";
+			$this->getLogin();
+			return;
+		}
+
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL				=>	"http://" . DIEA_EDSHARE_HOST . "/sword-app/servicedocument",
+			CURLOPT_POST			=>	false,
+			CURLOPT_HEADER			=>	true,
+			CURLOPT_USERPWD			=>	$_POST["diea_username"] . ":" . $_POST["diea_password"],
+			CURLOPT_RETURNTRANSFER	=>	true,
+			CURLOPT_HTTPHEADER		=>	array(
+				"Expect: ",
+			),
+		));
+		$response = curl_exec($curl);
+		$responseinfo = curl_getinfo($curl);
+		$code = $responseinfo["http_code"];
+		$headers = response_headers($response);
+		$body = response_body($response);
+
+		switch ($code) {
+			case 401:
+				// bad username/password
+				unset($_SESSION["diea_username"], $_SESSION["diea_password"], $_SESSION["diea_servicedocxml"]);
+				$this->errors[] = "There was an authorization problem" . (isset($headers["X-Error-Code"]) ? ": " . $headers["X-Error-Code"] : "");
+				$this->getLogic();
+				return;
+			case 200:
+				// parse XML response
+				$_SESSION["diea_username"] = $_POST["diea_username"];
+				$_SESSION["diea_password"] = $_POST["diea_password"];
+				$_SESSION["diea_servicedocxml"] = $body;
+
+				$this->getLogic();
+				return;
+			default:
+				// unexpected response
+				$this->errors[] = "Unexpected response: " . $responseinfo["http_code"] . ". Response headers follow:";
+				foreach ($headers as $k => $v)
+					$this->errors[] = "$k: $v";
+				$this->getLogic();
+				return;
+		}
 	}
 }
 
