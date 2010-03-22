6165af7c5b6da2d295efe213526f5f7d8be759e7
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-03-22 11:09:03 +0000

    bumped version number to 0.5~git for development

--- include/constants.php
+++ include/constants.php
@@ -13,7 +13,7 @@ licence -- see the LICENCE file for more details
 
 // program
 define("PROGRAMNAME", "Eqiat");
-define("VERSION", "0.4");
+define("VERSION", "0.5~git");
 
 // filesystem path to the eqiat root directory -- one level above this file, 
 // ending in a trailing slash
