2ba19f5e56cccd34802b318fd8fb61d51ce923ba
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-04-08 15:02:30 +0100

    added link to main menu to start new search or continue existing

--- content/mainMenu.php
+++ content/mainMenu.php
@@ -14,9 +14,13 @@ include "htmlheader.php";
 ?>
 
 <dl id="mainmenu">
-	<dt><a href="<?php echo SITEROOT_WEB; ?>?page=itemList">Item list</a></dt>
-	<dd>A filterable list of all items currently in <?php echo 
-	htmlspecialchars(SITE_TITLE); ?></dd>
+	<dt><a href="<?php echo SITEROOT_WEB; ?>?page=itemList&amp;clear=true">Item list</a></dt>
+	<dd>A filterable list of all items currently in <?php echo htmlspecialchars(SITE_TITLE); ?> &ndash; start a new search</dd>
+
+	<?php if (isset($_SESSION["search"])) { ?>
+		<dt><a href="<?php echo SITEROOT_WEB; ?>?page=itemList">Filtered item list</a></dt>
+		<dd>Return to your previous item list search results</dd>
+	<?php } ?>
 </dl>
 
 <?php include "htmlfooter.php"; ?>
