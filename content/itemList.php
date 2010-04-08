b8d670ee79dacfa749bed5de0ff5705131124e99
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-04-08 14:59:31 +0100

    search and results are now saved to session

--- content/itemList.php
+++ content/itemList.php
@@ -10,69 +10,101 @@ Licensed under the Creative Commons 'Attribution non-commercial share alike'
 licence -- see the LICENCE file for more details
 ------------------------------------------------------------------------------*/
 
-// get ordering settings from request vars
+// use old search unless filters or clear action are set in request vars
+$newsearch = isset($_REQUEST["clear"]) || isset($_REQUEST["direction"]) || isset($_REQUEST["orderby"]) || isset($_REQUEST["user"]) || isset($_REQUEST["keyword"]);
+
+// defaults
 $direction = "ASC";
-if (isset($_REQUEST["direction"]) && $_REQUEST["direction"] == "DESC")
-	$direction = "DESC";
 
 // default sort is by modified or uploaded date
 $orderby = "modified";
 $orderbysql = "COALESCE(modified, uploaded)";
 
-// override if set in request vars
-if (isset($_REQUEST["orderby"]) && !empty($_REQUEST["orderby"])) switch ($_REQUEST["orderby"]) {
-	case "uploaded":
-	case "user":
-	case "title":
-	case "description":
-		$orderbysql = $_REQUEST["orderby"];
-		$orderby = $_REQUEST["orderby"];
-		break;
-}
-
-// if order not set in request vars and we're ordering by date, show newest 
-// first
-if (!isset($_REQUEST["direction"]) || empty($_REQUEST["direction"]) && (!isset($_REQUEST["orderby"]) || empty($_REQUEST["orderby"]) || $_REQUEST["orderby"] == "modified" || $_REQUEST["orderby"] == "uploaded"))
-	$direction = "DESC";
-
-// filter by user and keyword
+// empty where clause
+$keyword = null;
+$user = null;
 $where = array();
-if (isset($_REQUEST["user"]) && !empty($_REQUEST["user"]))
-	$where[] = "user='" . db()->escapeString($_REQUEST["user"]) . "'";
-if (isset($_REQUEST["keyword"]) && !empty($_REQUEST["keyword"]))
-	$where[] = "keywords.keyword='" . db()->escapeString($_REQUEST["keyword"]) . "'";
 
-// TODO: limits (pagination)
+if ($newsearch) {
+	if (isset($_REQUEST["clear"]))
+		unset($_SESSION["search"]);
+	else {
+		// get ordering settings from request vars
 
-// get items from database
-$sql = "
-	SELECT items.*
-	FROM items
-	LEFT JOIN keywords ON items.identifier=keywords.item
-	" . (empty($where) ? "" : "WHERE " . implode(" AND ", $where)) . "
-	GROUP BY keywords.item
-	ORDER BY $orderbysql $direction
-;";
-$result = db()->query($sql);
-$items = array();
-while ($row = $result->fetchArray(SQLITE3_ASSOC))
-	$items[$row["identifier"]] = $row;
+		// override sort direction
+		if (isset($_REQUEST["direction"]) && $_REQUEST["direction"] == "DESC")
+			$direction = "DESC";
 
-// get keywords for the items we've collected
-if (!empty($items)) {
+		// override sort
+		if (isset($_REQUEST["orderby"]) && !empty($_REQUEST["orderby"])) switch ($_REQUEST["orderby"]) {
+			case "uploaded":
+			case "user":
+			case "title":
+			case "description":
+				$orderbysql = $_REQUEST["orderby"];
+				$orderby = $_REQUEST["orderby"];
+				break;
+		}
+
+		// if order not set in request vars and we're ordering by date, show 
+		// newest first
+		if (!isset($_REQUEST["direction"]) || empty($_REQUEST["direction"]) && (!isset($_REQUEST["orderby"]) || empty($_REQUEST["orderby"]) || $_REQUEST["orderby"] == "modified" || $_REQUEST["orderby"] == "uploaded"))
+			$direction = "DESC";
+
+		// filter by user and keyword
+		if (isset($_REQUEST["user"]) && !empty($_REQUEST["user"])) {
+			$user = $_REQUEST["user"];
+			$where[] = "user='" . db()->escapeString($user) . "'";
+		}
+		if (isset($_REQUEST["keyword"]) && !empty($_REQUEST["keyword"])) {
+			$keyword = $_REQUEST["keyword"];
+			$where[] = "keywords.keyword='" . db()->escapeString($keyword) . "'";
+		}
+
+		// save that stuff in the session
+		$_SESSION["search"] = array(
+			"direction"	=>	$direction,
+			"orderby"	=>	$orderby,
+			"orderbysql"	=>	$orderbysql,
+			"where"	=>	$where,
+			"user"	=>	$user,
+			"keyword"	=>	$keyword,
+		);
+	}
+
+	// get items IDs from database and store in the session
 	$sql = "
-		SELECT *
-		FROM keywords
-		WHERE item IN ('" . implode("', '", array_keys($items)) . "')
-		ORDER BY keyword ASC
+		SELECT items.identifier
+		FROM items
+		LEFT JOIN keywords ON items.identifier=keywords.item
+		" . (empty($where) ? "" : "WHERE " . implode(" AND ", $where)) . "
+		GROUP BY keywords.item
+		ORDER BY $orderbysql $direction
 	;";
 	$result = db()->query($sql);
-	while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-		if (!isset($items[$row["item"]]["keywords"]))
-			$items[$row["item"]]["keywords"] = array();
-		$items[$row["item"]]["keywords"][] = $row["keyword"];
-	}
+	$_SESSION["items"] = array();
+	while ($row = $result->fetchArray(SQLITE3_NUM))
+		$_SESSION["items"][] = $row[0];
+} else if (isset($_SESSION["search"])) {
+	// recover those vars set above
+	if (isset($_SESSION["search"]["direction"]))
+		$direction = $_SESSION["search"]["direction"];
+	if (isset($_SESSION["search"]["orderby"]))
+		$orderby = $_SESSION["search"]["orderby"];
+	if (isset($_SESSION["search"]["orderbysql"]))
+		$orderbysql = $_SESSION["search"]["direction"];
+	if (isset($_SESSION["search"]["where"]))
+		$where = $_SESSION["search"]["where"];
+	if (isset($_SESSION["search"]["user"]))
+		$user = $_SESSION["search"]["user"];
+	if (isset($_SESSION["search"]["keyword"]))
+		$keyword = $_SESSION["search"]["keyword"];
 }
+
+// get items
+$items = array();
+foreach ($_SESSION["items"] as $itemid)
+	$items[$itemid] = getitem($itemid);
 
 $title = "Item list";
 include "htmlheader.php";
@@ -86,7 +118,7 @@ include "htmlheader.php";
 		<tr>
 			<?php foreach (array("uploaded", "modified", "user", "title", "description") as $type) { ?>
 				<th<?php if ($orderby == $type) { ?> class="ordered"<?php } ?>>
-					<a href="<?php echo Uri::construct(true)->addvars(array(
+					<a href="<?php echo Uri::construct(true)->removevars("clear")->addvars(array(
 						"orderby" => $type,
 						"direction" => $orderby == $type && $direction == "ASC" ? "DESC" : "ASC",
 					))->geturi(true); ?>">
@@ -106,10 +138,10 @@ include "htmlheader.php";
 				<td><?php echo htmlspecialchars($item["description"]); ?></td>
 				<td><?php if (!empty($item["keywords"])) { ?>
 					<ul class="keywords">
-						<?php foreach ($item["keywords"] as $keyword) { ?>
+						<?php foreach ($item["keywords"] as $itemkeyword) { ?>
 							<li>
-								<a href="<?php echo Uri::construct(true)->addvars("keyword", $keyword)->geturi(true); ?>">
-									<?php echo htmlspecialchars($keyword); ?>
+								<a href="<?php echo Uri::construct(true)->removevars("clear")->addvars("keyword", $itemkeyword)->geturi(true); ?>">
+									<?php echo htmlspecialchars($itemkeyword); ?>
 								</a>
 							</li>
 						<?php } ?>
@@ -138,10 +170,10 @@ include "htmlheader.php";
 <form action="<?php echo SITEROOT_WEB; ?>" method="get">
 	<dl>
 		<dt><label for="user">User</label></dt>
-		<dd><input type="text" name="user" id="user" size="64"<?php if (isset($_REQUEST["user"])) { ?> value="<?php echo htmlspecialchars($_REQUEST["user"]); ?>"<?php } ?>></dd>
+		<dd><input type="text" name="user" id="user" size="64"<?php if (isset($user)) { ?> value="<?php echo htmlspecialchars($user); ?>"<?php } ?>></dd>
 
 		<dt><label for="keyword">Keyword</label></dt>
-		<dd><input type="text" name="keyword" id="keyword" size="64"<?php if (isset($_REQUEST["keyword"])) { ?> value="<?php echo htmlspecialchars($_REQUEST["keyword"]); ?>"<?php } ?>></dd>
+		<dd><input type="text" name="keyword" id="keyword" size="64"<?php if (isset($keyword)) { ?> value="<?php echo htmlspecialchars($keyword); ?>"<?php } ?>></dd>
 
 		<dt><label for="orderby">Order by</label></dt>
 		<dd>
@@ -166,6 +198,9 @@ include "htmlheader.php";
 		<dd>
 			<input type="hidden" name="page" value="itemList">
 			<input type="submit" name="filter" value="Filter">
+			<?php if (!empty($where)) { ?>
+				<input type="submit" name="clear" value="Clear filters">
+			<?php } ?>
 		</dd>
 	</dl>
 </form>
