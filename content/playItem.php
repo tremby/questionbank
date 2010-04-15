2e805ea1ce7b423dc0dfa7e123b65e746c89bc7f
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-04-15 19:16:14 +0100

    more work on playItem. QTIEngineWS's newSession call is useless -- get session from capturing the Set-Cookie header.

--- content/playItem.php
+++ content/playItem.php
@@ -10,54 +10,74 @@ Licensed under the Creative Commons 'Attribution non-commercial share alike'
 licence -- see the LICENCE file for more details
 ------------------------------------------------------------------------------*/
 
-// decide what to do
+// URL to embed in QTIEngine XML
+$actionurl = SITEROOT_WEB . "?page=playItem";
+
+// if QTIEngine form submitted post onwards to QTIEngine and display its output
 if (isset($_POST["submit"])) {
-	// QTIEngine form submitted -- post onwards to QTIEngine
-	// TODO
-} else {
-	$action = isset($_GET["action"]) ? $_GET["action"] : null;
-	switch ($action) {
-		case "results":
-			// set item queue to current search results
-			if (!isset($_SESSION["items"]) || empty($_SESSION["items"]))
-				badrequest("no search results");
-			$_SESSION["itemqueue"] = $_SESSION["items"];
-			$_SESSION["itemqueuepos"] = 0;
-			break;
-		case "single":
-			// set item queue to the single specified item
-			if (!isset($_GET["qtiid"]))
-				badrequest("no QTI ID specified");
-			$_SESSION["itemqueue"] = array($_GET["qtiid"]);
-			$_SESSION["itemqueuepos"] = 0;
-			break;
-		case "next":
-			// move the item pointer on and check if we're finished
-			if (++$_SESSION["itemqueuepos"] >= count($_SESSION["itemqueue"])) {
-				$title = "Finished";
-				include "htmlheader.php";
-				?>
-				<h1><?php echo htmlspecialchars($title); ?></h1>
-				<?php if (count($_SESSION["itemqueue"]) == 1) { ?>
-					<p>You've finished the only item in the queue.</p>
-				<?php } else { ?>
-					<p>You've got to the end of the <?php echo count($_SESSION["itemqueue"]); ?> items in the queue.</p>
-				<?php } ?>
-				<p>What do you want to do now?</p>
-				<ul>
-					<li><a href="<?php echo SITEROOT_WEB; ?>">Go back to the main menu</a></li>
-					<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=startover">Take <?php echo plural($_SESSION["itemqueue"], "these items", "this item"); ?> again</a></li>
-				</ul>
-				<?php
-				include "htmlfooter.php";
-				exit;
-			}
-			break;
-		case "startover":
-			// reset the item pointer
-			$_SESSION["itemqueuepos"] = 0;
-			break;
-	}
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL				=>	"http://" . QTIENGINE_HOST . ":" . QTIENGINE_PORT . QTIENGINE_PATH . "rest/playItem/0;jsessionid=" . $_SESSION["qtiengine_session"],
+		CURLOPT_POST			=>	true,
+		CURLOPT_RETURNTRANSFER	=>	true,
+		CURLOPT_POSTFIELDS		=>	array_merge(array("actionUrl" => $actionurl), $_POST),
+	));
+	$response = curl_exec($curl);
+
+	$xml = new SimpleXMLElement($response) or servererror("couldn't parse XML response");
+
+	// TODO: get scripts and stylesheets from HTML header -- find some way to make 
+	// Jquery and Prototype play together
+
+	echo simplexml_indented_string($xml->page->html);
+	exit;
+}
+
+// decide what to do
+$action = isset($_GET["action"]) ? $_GET["action"] : null;
+switch ($action) {
+	case "results":
+		// set item queue to current search results
+		if (!isset($_SESSION["items"]) || empty($_SESSION["items"]))
+			badrequest("no search results");
+		$_SESSION["itemqueue"] = $_SESSION["items"];
+		$_SESSION["itemqueuepos"] = 0;
+		break;
+	case "single":
+		// set item queue to the single specified item
+		if (!isset($_GET["qtiid"]))
+			badrequest("no QTI ID specified");
+		$_SESSION["itemqueue"] = array($_GET["qtiid"]);
+		$_SESSION["itemqueuepos"] = 0;
+		break;
+	case "next":
+		// move the item pointer on and check if we're finished
+		if (++$_SESSION["itemqueuepos"] >= count($_SESSION["itemqueue"])) {
+			$title = "Finished";
+			include "htmlheader.php";
+			?>
+			<h1><?php echo htmlspecialchars($title); ?></h1>
+			<?php if (count($_SESSION["itemqueue"]) == 1) { ?>
+				<p>You've finished the only item in the queue.</p>
+			<?php } else { ?>
+				<p>You've got to the end of the <?php echo count($_SESSION["itemqueue"]); ?> items in the queue.</p>
+			<?php } ?>
+			<p>What do you want to do now?</p>
+			<ul>
+				<li><a href="<?php echo SITEROOT_WEB; ?>">Go back to the main menu</a></li>
+				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=startover">Take <?php echo plural($_SESSION["itemqueue"], "these items", "this item"); ?> again</a></li>
+			</ul>
+			<?php
+			include "htmlfooter.php";
+			exit;
+		}
+		break;
+	case "startover":
+		// reset the item pointer
+		$_SESSION["itemqueuepos"] = 0;
+		break;
+	default:
+		badrequest("no action given");
 }
 
 // display a new item
@@ -66,9 +86,6 @@ if (isset($_POST["submit"])) {
 $item = getitem(array_shift($_SESSION["itemqueue"]));
 if (!$item)
 	badrequest("no item with the given QTI ID exists in the database");
-
-// get a new QTIEngine session ID
-$_SESSION["qtiengine_session"] = file_get_contents("http://" . QTIENGINE_HOST . ":" . QTIENGINE_PORT . QTIENGINE_PATH . "rest/newSession") or servererror("couldn't get QTIEngine session");
 
 // upload the QTI to QTIEngine
 // Doing this manually rather than using curl because until PHP 5.2.7 (SVN 
@@ -86,13 +103,9 @@ while (true) {
 
 // request
 $request =	"--$boundary\r\n";
-$request .=	"Content-Disposition: form-data; name=\"jsession\"\r\n";
-$request .=	"\r\n";
-$request .=	$_SESSION["qtiengine_session"] . "\r\n";
-$request .=	"--$boundary\r\n";
 $request .=	"Content-Disposition: form-data; name=\"actionUrl\"\r\n";
 $request .=	"\r\n";
-$request .=	SITEROOT_WEB . "?page=playItem\r\n";
+$request .=	"$actionurl\r\n";
 $request .=	"--$boundary\r\n";
 $request .=	"Content-Disposition: form-data; name=\"uploadedContent\"; filename=\"qb_" . $item["identifier"] . ".xml\"\r\n";
 $request .=	"Content-Type: application/xml\r\n";
@@ -138,6 +151,14 @@ while (true) {
 		}
 	}
 
+	// get the session id from the Set-Cookie header
+	if (isset($header["Set-Cookie"])) {
+		$cookieparts = explode(";", $header["Set-Cookie"]);
+		list($name, $value) = explode("=", $cookieparts[0]);
+		if ($name == "JSESSIONID")
+			$_SESSION["qtiengine_session"] = $value;
+	}
+
 	$response = "";
 
 	// get rest of response and stop if HTTP response code is not a redirection
@@ -177,7 +198,7 @@ while (true) {
 	}
 
 	// redirect
-	$url = $urlparts["path"] . "?" . $urlparts["query"];
+	$url = preg_replace('%;jsessionid=.*$%', ";jsessionid=" . $_SESSION["qtiengine_session"], $urlparts["path"]) . "?" . $urlparts["query"];
 	$reqaction = "GET $url HTTP/1.1";
 
 	// delete POST related headers
