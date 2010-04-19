2bda7fa0a32459c04100bc793c067541d0c3c4bf
Bart Nagel <bjn@ecs.soton.ac.uk>
2010-04-19 20:01:26 +0100

    tidied the main menu

--- content/mainMenu.php
+++ content/mainMenu.php
@@ -29,23 +29,25 @@ include "htmlheader.php";
 	</dd>
 	<dt>Play items</dt>
 	<dd>
-		Play a set of items
-		<ul>
-			<?php if (isset($_SESSION["itemqueue"])) { ?>
+		<?php if (isset($_SESSION["itemqueue"]) && !empty($_SESSION["itemqueue"])) { ?>
+			<ul>
 				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem">Return to current set of items</a> (<?php echo count($_SESSION["itemqueue"]); ?> item<?php echo plural($_SESSION["itemqueue"]); ?>)</li>
-			<?php } ?>
+			</ul>
+		<?php } ?>
+		Play all items...
+		<ul>
 			<?php if (isset($_SESSION["search"]) && count($_SESSION["items"]) > 0 && (!isset($_SESSION["itemqueue"]) || $_SESSION["items"] != $_SESSION["itemqueue"])) { ?>
-				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=results">Play the current search results</a> (<?php echo count($_SESSION["items"]); ?> item<?php echo plural($_SESSION["items"]); ?>)</li>
+				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=results">in the current search results</a> (<?php echo count($_SESSION["items"]); ?> item<?php echo plural($_SESSION["items"]); ?>)</li>
 			<?php } ?>
-			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=shuffle">Shuffle all items in <?php echo htmlspecialchars(SITE_TITLE); ?></a></li>
-			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=newest">Play all items in <?php echo htmlspecialchars(SITE_TITLE); ?> from newest to oldest</a></li>
-			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=highestrated">Play all items in <?php echo htmlspecialchars(SITE_TITLE); ?> from highest rated to lowest</a></li>
+			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=shuffle">at random</a></li>
+			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=newest">from newest to oldest</a></li>
+			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=highestrated">from highest rated to lowest</a></li>
 			<?php if (loggedin()) { ?>
-				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=unratedbyuser">Play all items in <?php echo htmlspecialchars(SITE_TITLE); ?> you haven't yet rated, oldest first</a></li>
+				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=unratedbyuser">you haven't yet rated, oldest first</a></li>
 			<?php } ?>
-			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=unrated">Play all items in <?php echo htmlspecialchars(SITE_TITLE); ?> which haven't been rated by anyone, oldest first</a></li>
+			<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=unrated">which haven't been rated by anyone, oldest first</a></li>
 			<?php if (loggedin()) { ?>
-				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=notbyuser">Play all items in <?php echo htmlspecialchars(SITE_TITLE); ?> which weren't uploaded by you</a></li>
+				<li><a href="<?php echo SITEROOT_WEB; ?>?page=playItem&amp;action=notbyuser">you didn't upload</a></li>
 			<?php } ?>
 		</ul>
 		You can build other queues of items by searching the item list
